b5b2597547b415e0b6f1bb85b359bbda0759e3f5
Harshitha Dwaracherla <harshitha@berkeley.edu>
2024-12-23 23:20:18 -0800

    Time: 0 ms (100%), Space: 18 MB (6.47%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,19 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        res = ""
+        count = 1
+        prev = chars[0]
+        
+        for i in range(1, len(chars)):
+            if chars[i] == prev:
+                count += 1
+            else:
+                res += prev + (str(count) if count > 1 else "")
+                prev = chars[i]
+                count = 1 
+        
+        # Handle the last group of characters
+        res += prev + (str(count) if count > 1 else "")
+        
+        chars[:len(res)] = list(res)
+        return len(res)
