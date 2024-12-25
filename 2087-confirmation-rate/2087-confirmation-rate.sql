29190c5b45ba7e3c0cf6d116924003a5dfbb61ec
Harshitha Dwaracherla <harshitha@berkeley.edu>
2024-12-24 21:14:36 -0800

    Time: 1747 ms (5%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,6 +1,7 @@
 ## be cautious in picking the user_id from the correct table
 
-SELECT S.user_id, IFNULL(ROUND(SUM(C.action="Confirmed")/COUNT(*), 2), 0) AS confirmation_rate
+SELECT S.user_id, 
+IFNULL(ROUND(SUM(C.action="Confirmed")/COUNT(*), 2), 0) AS confirmation_rate
 FROM Confirmations AS C
 RIGHT JOIN Signups AS S
 ON C.user_id = S.user_id
