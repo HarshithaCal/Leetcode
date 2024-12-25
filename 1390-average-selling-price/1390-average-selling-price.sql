b1631fa4901aa60668913c0aa28b76ab6740cad5
Harshitha Dwaracherla <harshitha@berkeley.edu>
2024-12-24 21:56:23 -0800

    Time: 1392 ms (12.03%), Space: 0B (100%) - LeetHub

---
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -0,0 +1,8 @@
+SELECT p.product_id, 
+IFNULL(ROUND(SUM(p.price*u.units)/SUM(u.units),2),0) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id
+WHERE u.purchase_date between p.start_date AND p.end_date 
+                OR u.purchase_date IS NULL #Eventhough LEFT JOIN is used WHERE misses the null cases.
+GROUP BY p.product_id
