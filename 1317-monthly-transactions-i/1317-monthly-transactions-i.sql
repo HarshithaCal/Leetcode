0f168a0d39fcb160b35afc2bf42f1411f4cf6ece
Harshitha Dwaracherla <harshitha@berkeley.edu>
2024-12-25 23:52:32 -0800

    Time: 569 ms (87.75%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -1,10 +1,14 @@
 SELECT 
-LEFT(trans_date, 7) AS month,
+-- LEFT(trans_date, 7) AS month,
+DATE_FORMAT(trans_date, '%Y-%m') AS month,
+-- SUBSTR(trans_date,1,7) as month,
 country,
-COUNT(id) as trans_count,
+COUNT(*) as trans_count,
 SUM(state = "approved") as approved_count,
+-- SUM(CASE WHEN state = 'approved' then 1 else 0 END) as approved_count, 
 SUM(amount) as trans_total_amount,
 SUM((state = "approved")*amount) AS approved_total_amount
+-- SUM(CASE WHEN state = 'approved' then amount else 0 END) as approved_total_amount
+-- SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
 FROM Transactions 
 GROUP BY month, country;
-
