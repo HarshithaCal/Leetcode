49d871c93c0b1eca446cb35827e5d1d5a5cb071c
Harshitha Dwaracherla <harshitha@berkeley.edu>
2024-12-26 21:32:21 -0800

    Time: 562 ms (22.63%), Space: 0B (100%) - LeetHub

--- 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -1,5 +1,7 @@
 SELECT query_name, 
-ROUND(SUM(rating/position)/COUNT(rating),2) AS quality,
+-- ROUND(SUM(rating/position)/COUNT(rating),2) AS quality,
+round(avg(rating / position), 2) as quality,
 ROUND(100 * SUM(IF(rating<3, 1, 0))/COUNT(*), 2) AS poor_query_percentage
+-- round(sum(case when rating < 3 then 1 else 0 end) * 100 / count(*), 2) as poor_query_percentage
 FROM Queries
 GROUP BY query_name;
