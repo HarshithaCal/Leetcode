64e09131d5d89934521e9ec7a37e0d8a790bf6eb
Harshitha Dwaracherla <harshitha@berkeley.edu>
2024-12-30 05:10:58 -0800

    Time: 369 ms (61.4%), Space: 0B (100%) - LeetHub

--- 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -1,10 +1,18 @@
 # Write your MySQL query statement below
+#using subquery
+-- SELECT e1.employee_id FROM Employees e1
+-- WHERE e1.salary < 30000 
+-- AND e1.manager_id
+--     NOT IN 
+--         (SELECT DISTINCT e2.employee_id FROM Employees e2) #list of employees still present in the company
+-- ORDER BY e1.employee_id;
 
-SELECT e1.employee_id FROM Employees e1
-WHERE e1.salary < 30000 
-AND e1.manager_id
-    NOT IN 
-        (SELECT DISTINCT e2.employee_id FROM Employees e2) #list of employees still present in the company
-ORDER BY e1.employee_id;
+#using join
+SELECT e1.employee_id
+FROM Employees e1
+LEFT JOIN Employees e2
+ON e1.manager_id = e2.employee_id
+WHERE e1.salary < 30000 AND e2.employee_id IS NULL AND e1.manager_id IS NOT NULL
+ORDER BY employee_id;
 
 
