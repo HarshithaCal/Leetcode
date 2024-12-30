7ea048c93c6a203bcc33b8155396fdad5821c49a
Harshitha Dwaracherla <harshitha@berkeley.edu>
2024-12-30 03:23:14 -0800

    Time: 45 ms (85.76%), Space: 49.4 MB (5.52%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.py
@@ -5,36 +5,52 @@
 #         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        #find the mid node number
-        length = 0
-        p = head
-        if head:
-            length += 1
-            p = p.next
-        while p:
-            length += 1
-            p = p.next
+        #optimized approach using fast and slow 
+        if head == None:
+            return None
+        prev = ListNode() #prev to the curr node so dummy node before head
+        prev.next = head
+        slow = prev
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
         
-        mid = floor(length/2)
-        print("mid index  value is ", mid)
-
-        #Corner case where there is only 1 element
-        if mid == 0:
-            head = None
-            return head
+        slow.next = slow.next.next
+        return prev.next
 
 
-        count = 1
-        p1 = head
 
-        # get p1 such that p1.next is the mid index
-        while count< mid:
-            p1 = p1.next
-            count += 1
+        # #find the mid node number
+        # length = 0
+        # p = head
+        # if head:
+        #     length += 1
+        #     p = p.next
+        # while p:
+        #     length += 1
+        #     p = p.next
         
-        q = p1.next
-        p1.next = q.next
-        return head
+        # mid = floor(length/2)
+        # print("mid index  value is ", mid)
+
+        # #Corner case where there is only 1 element
+        # if mid == 0:
+        #     head = None
+        #     return head
+
+
+        # count = 1
+        # p1 = head
+
+        # # get p1 such that p1.next is the mid index
+        # while count< mid:
+        #     p1 = p1.next
+        #     count += 1
+        
+        # q = p1.next
+        # p1.next = q.next
+        # return head
 
 
 
