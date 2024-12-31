6bb649ff68f0e9bb8e6076ae63865d9e1c9ac10b
Harshitha Dwaracherla <harshitha@berkeley.edu>
2024-12-31 06:17:34 -0800

    Time: 343 ms (81.11%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,14 +1,22 @@
-select 
-    case  
-        when id = (select max(id) from seat) and id % 2 = 1
-            then id
+-- select 
+--     case  
+--         when id = (select max(id) from seat) and id % 2 = 1
+--             then id
 
-        when id % 2 = 1
-            then id + 1
+--         when id % 2 = 1
+--             then id + 1
 
-        when id % 2 = 0
-            then id - 1
-    end as id,
-    student
-from seat
-order by id;
+--         when id % 2 = 0
+--             then id - 1
+--     end as id,
+--     student
+-- from seat
+-- order by id;
+
+SELECT 
+    id,
+    CASE
+        WHEN id % 2 = 0 THEN LAG(student) OVER(ORDER BY id)
+        ELSE COALESCE(LEAD(student) OVER(ORDER BY id), student)
+    END AS student
+FROM Seat
