8e80e60be4adef8c589da01bf81f4ee97f471491
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-07 00:02:40 -0800

    Time: 426 ms (37.52%), Space: 0B (100%) - LeetHub

--- 3530-odd-and-even-transactions/3530-odd-and-even-transactions.sql
+++ 3530-odd-and-even-transactions/3530-odd-and-even-transactions.sql
@@ -1,10 +1,18 @@
+select  
+        transaction_date,
+        coalesce(sum(case when mod(amount,2)=1 then amount end),0) as odd_sum,   #coalesce() fills NULL with the value
+        coalesce(sum(case when mod(amount,2)=0 then amount end),0) as even_sum
+    from transactions
+    group by 1  #col number
+    order by 1
+
 #### Using IF statement
-SELECT transaction_date,
- sum(if(amount % 2 = 1, amount, 0)) as odd_sum,
- sum(if(amount % 2 = 0, amount, 0)) as even_sum
-from transactions
-GROUP BY transaction_date 
-ORDER BY transaction_date;
+-- SELECT transaction_date,
+--  sum(if(amount % 2 = 1, amount, 0)) as odd_sum,
+--  sum(if(amount % 2 = 0, amount, 0)) as even_sum
+-- from transactions
+-- GROUP BY transaction_date 
+-- ORDER BY transaction_date;
 
 
 
