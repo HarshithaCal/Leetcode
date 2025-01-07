f5062f2540faf99e1f1383d2166dbe6d34ff91cb
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-07 02:21:08 -0800

    Time: 323 ms (25.86%), Space: 25.5 MB (31%) - LeetHub

--- 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
+++ 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -4,26 +4,67 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+from collections import deque
 
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
-        #pre-order
-
-        # print(f"original: {original} and cloned: {cloned}")
+        ### Iterative: BFS. BFS means queue
+        # BFS - root, left, right
+        # Initialize queues for BFS traversal
+        queue_original = deque([original])
+        queue_cloned = deque([cloned])
         
-        if original is None:
-            return None
-
-        # if original == cloned == target:  -- #original and cloned are different tree nodes so this is not satisfied
-        if original == target:
-            return cloned
+        while queue_original:
+            # Get the current nodes from both trees
+            current_original = queue_original.popleft()
+            current_cloned = queue_cloned.popleft()
+            
+    
+            if current_original == target:
+                return current_cloned 
+            
+            # Add left children to the queue
+            if current_original.left:
+                queue_original.append(current_original.left)
+                queue_cloned.append(current_cloned.left)
+            
+            # Add right children to the queue
+            if current_original.right:
+                queue_original.append(current_original.right)
+                queue_cloned.append(current_cloned.right)
         
-        
-        left_result = self.getTargetCopy(original.left, cloned.left, target)
-        if left_result is not None:
-            return left_result
+        return None  # If target is not found (unlikely based on problem description)
 
-        return self.getTargetCopy(original.right, cloned.right, target)
+
+
+
+
+
+        # #########pre-order + DFS. DFS means stack
+        # if original is None:
+        #     return None
+
+        # # if original == cloned == target:  -- #original and cloned are different tree nodes so this is not satisfied
+        # if original == target:
+        #     return cloned
+                
+        # left_result = self.getTargetCopy(original.left, cloned.left, target)
+        # if left_result is not None:
+        #     return left_result
+
+        # return self.getTargetCopy(original.right, cloned.right, target)
+
+        # ################## inorder - DFS
+        # def inorder(o: TreeNode, c: TreeNode):
+        #     if o:
+        #         inorder(o.left, c.left)
+        #         if o is target:
+        #             self.ans = c
+
+        #         inorder(o.right, c.right)
+                
+        # inorder(original, cloned)
+        # return self.ans 
     
         
 
