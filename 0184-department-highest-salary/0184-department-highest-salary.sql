90d4c518716791369a0d076cadd124440e3af8f6
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-06 23:40:11 -0800

    Time: 1103 ms (34.38%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -11,18 +11,34 @@
 
 
 -- CTE - Approach 1
-WITH cte AS
-(
-    SELECT departmentId, MAX(salary) 
-    FROM Employee
-    GROUP BY departmentId 
+-- WITH cte AS
+-- (
+--     SELECT departmentId, MAX(salary) 
+--     FROM Employee
+--     GROUP BY departmentId 
+-- )
+
+-- SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+-- FROM Department d
+-- JOIN Employee e
+-- ON e.departmentId = d.id
+-- WHERE (d.id, salary) IN (SELECT * FROM cte)
+
+with cte as (
+    select name, salary, departmentId,
+    dense_rank() over(  partition by departmentId 
+                        order by 
+                        salary desc) as ranking 
+                        #dense rank as same salary - same rank and need not skip the ranking for the next one
+    from Employee
 )
 
-SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
-FROM Department d
-JOIN Employee e
-ON e.departmentId = d.id
-WHERE (d.id, salary) IN (SELECT * FROM cte)
+select d.name as Department, cte.name as Employee, cte.salary as Salary
+from cte 
+join Department d
+on cte.departmentId = d.id
+where cte.ranking = 1;
+
 
 
 
