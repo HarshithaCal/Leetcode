8286599f9a2a4e83ee7e016b8b149337868676a2
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-07 00:00:23 -0800

    Time: 286 ms (83.34%), Space: 0B (100%) - LeetHub

--- 3530-odd-and-even-transactions/3530-odd-and-even-transactions.sql
+++ 3530-odd-and-even-transactions/3530-odd-and-even-transactions.sql
@@ -1,12 +1,23 @@
-select transaction_date, 
-SUM(CASE WHEN amount %2 <> 0 THEN amount
-ELSE 0
-END) As odd_sum,
+#### Using IF statement
+SELECT transaction_date,
+ sum(if(amount % 2 = 1, amount, 0)) as odd_sum,
+ sum(if(amount % 2 = 0, amount, 0)) as even_sum
+from transactions
+GROUP BY transaction_date 
+ORDER BY transaction_date;
 
-SUM(CASE WHEN amount %2 = 0 THEN amount
-ELSE 0
-END) As even_sum
 
-FROM transactions
-GROUP BY transaction_date
-ORDER BY transaction_date
+
+### Using case statement
+-- select transaction_date, 
+-- SUM(CASE WHEN amount %2 <> 0 THEN amount
+-- ELSE 0
+-- END) As odd_sum,
+
+-- SUM(CASE WHEN amount %2 = 0 THEN amount
+-- ELSE 0
+-- END) As even_sum
+
+-- FROM transactions
+-- GROUP BY transaction_date
+-- ORDER BY transaction_date
