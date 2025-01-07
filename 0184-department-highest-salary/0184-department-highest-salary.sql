098ef82e6960e19a542b9895056589cb3be65854
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-06 23:31:11 -0800

    Time: 1479 ms (15.54%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,9 +1,31 @@
-select d.name AS Department, e.name AS Employee, e.salary AS Salary
+-- Subqueries
+-- select d.name AS Department, e.name AS Employee, e.salary AS Salary
+-- FROM Department d
+-- JOIN Employee e
+-- ON e.departmentId = d.id
+-- WHERE (d.id, salary) IN (
+--                                 SELECT departmentId, MAX(salary) 
+--                                 FROM Employee
+--                                 GROUP BY departmentId
+--                             )
+
+
+-- CTE
+WITH cte AS
+(
+    SELECT departmentId, MAX(salary) 
+    FROM Employee
+    GROUP BY departmentId 
+)
+
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
 FROM Department d
 JOIN Employee e
 ON e.departmentId = d.id
 WHERE (d.id, salary) IN (
-                                SELECT departmentId, MAX(salary) 
-                                FROM Employee
-                                GROUP BY departmentId
+                            SELECT * FROM cte
                             )
+
+
+
+
