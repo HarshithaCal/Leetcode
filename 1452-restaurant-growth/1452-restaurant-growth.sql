972392b93e30c80a0cd768abf0853710cbdecacd
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-09 09:10:22 -0800

    Time: 1894 ms (5%), Space: 0B (100%) - LeetHub

--- 1452-restaurant-growth/1452-restaurant-growth.sql
+++ 1452-restaurant-growth/1452-restaurant-growth.sql
@@ -1,15 +1,25 @@
-select visited_on, 
-SUM(amount)
-    OVER(ORDER BY visited_on ASC
-    ROWS BETWEEN 6 PRECEDING AND CURRENT ROW ) AS amount,
-ROUND(AVG(amount)
-    OVER( ORDER BY visited_on ASC
-    ROWS BETWEEN 6 PRECEDING AND CURRENT ROW ), 2) AS average_amount
-FROM (
-    select visited_on, SUM(amount) AS amount FROM Customer
-    GROUP BY visited_on
-        ) AS subQuery
-ORDER BY visited_on
-LIMIT 6, 9999
+WITH RankedResult AS
+    (   select visited_on, 
+        SUM(amount)
+            OVER(ORDER BY visited_on ASC
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW ) AS amount,
+        ROUND(AVG(amount)
+            OVER( ORDER BY visited_on ASC
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW ), 2) AS average_amount,
+        -- ROW_NUMBER() OVER (ORDER BY visited_on ASC) AS rn
+        RANK() OVER (ORDER BY visited_on ASC) AS rn
+
+        FROM (
+            select visited_on, SUM(amount) AS amount FROM Customer
+            GROUP BY visited_on
+                ) AS subQuery
+    )
+
+
+SELECT visited_on, amount, average_amount
+FROM RankedResult
+WHERE rn >= 7
+ORDER BY visited_on;
+
 
 
