23b8d8cd15c22b0f4e7aa72ea5c797c4b3832fc8
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-09 15:26:06 -0800

    Time: 665 ms (13.07%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+
+-- select id, max(`sum(counter)`) AS num FROM 
+-- (
+    with cte AS
+    (
+select id, sum(counter) AS total_requests FROM (
+select requester_id as id , COUNT(requester_id) AS counter 
+FROM RequestAccepted 
+GROUP BY id
+
+UNION ALL
+
+select accepter_id as id, COUNT(accepter_id) AS counter
+FROM RequestAccepted 
+GROUP BY id
+) as sub
+GROUP BY id
+    )
+
+select id, total_requests AS num from cte
+order by num desc
+limit 1
