33e1e0dd8b7ca07ec2d23cfa53c6f3fb14f3552b
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-09 12:13:15 -0800

    Time: 6 ms (61.71%), Space: 19.4 MB (8.13%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,20 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+    #XOR logic
+    #Boyer–Moore majority  
+        candidate = -1
+        votes = 0
+        for i in range(len(nums)):
+            if votes==0:
+                candidate = nums[i]
+                votes = 1
+            
+            else:
+                if nums[i] != candidate:
+                    votes -= 1
+                else:
+                    votes += 1
+            
+        return candidate
+
+            
