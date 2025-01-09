9bfe3798da7081fbac9dc716824cda23dd4a3641
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-09 10:43:10 -0800

    Time: 57 ms (30.98%), Space: 17.6 MB (33.29%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,23 +1,41 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i = 0
-        j = 1
-        counter = 0
-        print(len(nums))
-        while j<= len(nums)-1:
-            print(i, j, nums[i], nums[j])
-            if nums[i] == nums[j]:
-                counter+=1
-                if counter >1:
-                    nums.remove(nums[i])
-                    counter-=1
-                    continue
+        #total TC - O(N^2)
+        # i = 0
+        # j = 1
+        # counter = 0
+        # 
+        # while j<= len(nums)-1:
+        #     print(i, j, nums[i], nums[j])
+        #     if nums[i] == nums[j]:
+        #         counter+=1
+        #         if counter >1:
+        #             nums.remove(nums[i])  #TC - O(N)
+        #             counter-=1
+        #             continue
                                 
-            else:
-                counter = 0
+        #     else:
+        #         counter = 0
 
-            j+=1
-            i+=1
+        #     j+=1
+        #     i+=1
+
+        
+        ###########TC - O(N)
+        write_index = 1  # Position to overwrite the next non-duplicate
+        counter = 1  # To track the occurrences of the current element
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                counter += 1
+            else:
+                counter = 1
+
+            if counter <= 2:
+                nums[write_index] = nums[i]
+                write_index += 1
+
+        return write_index
 
             
         
