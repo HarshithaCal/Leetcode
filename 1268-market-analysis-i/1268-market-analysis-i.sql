ebc23102d0f7239bb575a7ebf5b98feb62009189
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-10 09:02:14 -0800

    Time: 1140 ms (91.96%), Space: 0B (100%) - LeetHub

--- 1268-market-analysis-i/1268-market-analysis-i.sql
+++ 1268-market-analysis-i/1268-market-analysis-i.sql
@@ -5,3 +5,12 @@ RIGHT JOIN Users u
 ON u.user_id = o.buyer_id
 -- WHERE LEFT(order_date, 4) = "2019"   ###### this is the issue to print 0's
 GROUP BY u.user_id, u.join_date
+
+
+-- ####Another solution:
+-- SELECT u.user_id as buyer_id, u.join_date, count(o.order_id) as 'orders_in_2019'
+-- FROM users u 
+-- LEFT JOIN Orders o 
+-- ON o.buyer_id=u.user_id 
+-- AND YEAR(order_date)='2019' 
+-- GROUP BY u.user_id
