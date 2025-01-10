aac57ad94599ce4478131b381390e10dc1113406
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-09 16:22:46 -0800

    Time: 0 ms (100%), Space: 18.1 MB (6.47%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,21 +1,42 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        n = len(nums)
 
-        # Edge case: Single element
-        if n == 1:
-            return 0
+        ##########tc - o(log n) - Binary search
+        left, right = 0, len(nums) - 1
 
-        # Edge case: Two elements
-        if n == 2:
-            return 0 if nums[0] > nums[1] else 1
+        while left < right:
+            mid = left + (right - left) // 2
 
-        for i in range(1, n - 1):
-            if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
-                return i
+            # Check the relationship between nums[mid] and nums[mid + 1]
+            if nums[mid] > nums[mid + 1]:
+                # Peak is in the left half (including mid)
+                right = mid
+            else:
+                # Peak is in the right half
+                left = mid + 1
 
-        # Check the boundaries
-        if nums[0] > nums[1]:
-            return 0
-        if nums[n - 1] > nums[n - 2]:
-            return n - 1
+        # Left and right converge to the peak element
+        return left
+
+
+
+        # #TC - O(N)
+        # n = len(nums)
+
+        # # Edge case: Single element
+        # if n == 1:
+        #     return 0
+
+        # # Edge case: Two elements
+        # if n == 2:
+        #     return 0 if nums[0] > nums[1] else 1
+
+        # for i in range(1, n - 1):
+        #     if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
+        #         return i
+
+        # # Check the boundaries - key!!
+        # if nums[0] > nums[1]:
+        #     return 0
+        # if nums[n - 1] > nums[n - 2]:
+        #     return n - 1
