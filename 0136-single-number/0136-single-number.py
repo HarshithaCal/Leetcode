900b5fe2aee018a2fbe5643be67ffe1e59510f4d
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-09 17:38:18 -0800

    Time: 0 ms (100%), Space: 19.5 MB (34.16%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -2,6 +2,6 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         res = 0
         for n in nums:
-            res = res ^ n
+            res = res ^ n #XOR of same no is 0
         return res
         
