c0ae5c57a640bf40b6c0c6e13097c33f25f6272b
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-13 12:57:06 -0800

    Time: 879 ms (5.03%), Space: 65.8 MB (71.21%) - LeetHub

--- 3070-fill-missing-data/3070-fill-missing-data.py
+++ 3070-fill-missing-data/3070-fill-missing-data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
-    products["quantity"]= products["quantity"].fillna(0)
+    # products["quantity"]= products["quantity"].fillna(0)
+    products.fillna({"quantity":0}, inplace = True)
     return products
