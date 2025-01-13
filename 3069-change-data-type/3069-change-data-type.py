c8cf2fc57c7da9ba4ae720a7b5e79af18e8d8185
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-13 12:53:27 -0800

    Time: 362 ms (63.01%), Space: 66.1 MB (53.52%) - LeetHub

--- 3069-change-data-type/3069-change-data-type.py
+++ 3069-change-data-type/3069-change-data-type.py
@@ -4,12 +4,12 @@ def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
     # print(students.dtypes)
 
     # #########good for multiple columns 
-    # convert_dict = {'grade': int}
-    # # Convert columns using the dictionary
-    # students = students.astype(convert_dict)
+    convert_dict = {'grade': int}
+    # Convert columns using the dictionary
+    students = students.astype(convert_dict)
 
     ####### for single col:
-    students['grade'] = students['grade'].astype(int)
+    # students['grade'] = students['grade'].astype(int)
     return students
 
     
