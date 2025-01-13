71114fc391ac20c2952335a8e43724c78f7e80c3
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-13 14:04:37 -0800

    Time: 650 ms (14.3%), Space: 67 MB (77.21%) - LeetHub

--- 3072-reshape-data-pivot/3072-reshape-data-pivot.py
+++ 3072-reshape-data-pivot/3072-reshape-data-pivot.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
     weather = weather.pivot(index=["month"], columns = ["city"], values="temperature")
+    # pivoted_df = weather.pivot_table(index='month', columns='city', values='temperature', aggfunc='mean')
     return weather
 
     
