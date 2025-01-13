ba00d3de41d16587dcf2cb16f8248a787ee28e67
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-13 14:13:00 -0800

    Time: 504 ms (34.73%), Space: 66.7 MB (28.5%) - LeetHub

--- 3073-reshape-data-melt/3073-reshape-data-melt.py
+++ 3073-reshape-data-melt/3073-reshape-data-melt.py
@@ -3,5 +3,9 @@ import pandas as pd
 def meltTable(report: pd.DataFrame) -> pd.DataFrame:
     df = pd.melt(report, id_vars=['product'], value_vars=['quarter_1', 'quarter_2',"quarter_3", "quarter_4"])
     df = df.rename(columns={"variable": "quarter", "value": "sales"})
+
+
+    #or 
+    # df = pd.melt(report, id_vars=['product'], var_name='quarter', value_name='sales')
     return df
     
