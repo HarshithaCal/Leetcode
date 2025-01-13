71c0b02f4b0ec472ad97e0ee53227d5ca9c4f839
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-13 12:13:39 -0800

    Time: 270 ms (93.79%), Space: 66.1 MB (36.07%) - LeetHub

--- 3065-display-the-first-three-rows/3065-display-the-first-three-rows.py
+++ 3065-display-the-first-three-rows/3065-display-the-first-three-rows.py
@@ -2,5 +2,15 @@ import pandas as pd
 
 def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
     # return employees.head(3)
-    return employees.loc[:2]
+
+    # pd.iloc:
+    # Integer-based indexing: It uses integer positions to access rows and columns.
+    # Syntax: df.iloc[row_index, column_index]
+    return employees.iloc[0:3,:]
+
+    # pd.loc:
+    # Label-based indexing: It uses labels (row and column names) to access data.
+    # Syntax: df.loc[row_label, column_label]
+    # return employees.loc[:2]
+    
     
