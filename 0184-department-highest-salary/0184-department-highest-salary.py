24d279e35a85c6414e4de3e7727cc35e9075eaf4
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-14 13:13:16 -0800

    Time: 583 ms (28.47%), Space: 68.3 MB (74.16%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.py
+++ 0184-department-highest-salary/0184-department-highest-salary.py
@@ -6,4 +6,10 @@ def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) 
     df = df[df["rank"] == 1][["name_y","name_x", "salary"]].rename(columns= {"name_y":"Department", "name_x":"Employee", "salary":"Salary"})
     return df
 
+##### Approach 2:
+    # join_tbl = pd.merge(employee, department, left_on = 'departmentId', right_on = 'id', how = 'left')
+    # max_salary = join_tbl.groupby('name_y')['salary'].transform('max')
+    # res = join_tbl[join_tbl.salary == max_salary]
+    # return(res[['name_y', 'name_x', 'salary']].rename(columns = {'name_y' : 'Department', 'name_x' : 'Employee', 'salary' : 'Salary'}))
+
     
