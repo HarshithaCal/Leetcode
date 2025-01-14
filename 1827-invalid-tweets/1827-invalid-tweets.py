bad19eb551ff4b39f32d272c9bca37154927b1a2
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-14 08:57:27 -0800

    Time: 493 ms (34.87%), Space: 66.3 MB (95.4%) - LeetHub

--- 1827-invalid-tweets/1827-invalid-tweets.py
+++ 1827-invalid-tweets/1827-invalid-tweets.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
 def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
-    df = tweets[tweets["content"].str.len() > 15][["tweet_id"]]
+    # df = tweets[tweets["content"].str.len() > 15][["tweet_id"]]
+    
     # df = tweets[len(tweets["content"]) > 15][["tweet_id"]] #len(tweets(["content"])) - doesn't work on each element seperately
-    return df
+    tweets["content_length"] = tweets["content"].apply(lambda row: len(row) > 15)
+    return tweets[tweets["content_length"]][["tweet_id"]]
+    
     
