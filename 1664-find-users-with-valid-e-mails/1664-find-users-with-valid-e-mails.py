a53210bc180115fc502406c09924bf7ac285b6ba
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-14 09:56:27 -0800

    Time: 697 ms (9.19%), Space: 66.5 MB (92.11%) - LeetHub

--- 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.py
+++ 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.py
@@ -9,5 +9,20 @@ def func(mail: str) -> bool:
     pattern = r"^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\.com$"
     return bool(re.match(pattern, mail))
 
+# def check(mail):
+#         if (mail[0].isalpha()) and ('@' in mail) and (mail[mail.index('@'):] == '@leetcode.com'):
+#             for i in mail[:mail.index('@')]:
+#                 if not (i.isalnum() or (i in ['_','.','-'])):
+#                     return False
+#             return True
+#         else:
+#             return False
+#     return users[users['mail'].apply(lambda x:check(x))]
+
+
+######Another approach
+    # result_df = users[users['mail'].str.match(r'^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\.com')]
+    # return result_df
+
     
     
