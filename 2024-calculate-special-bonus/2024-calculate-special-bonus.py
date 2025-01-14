cf20de63ed43c75410ef3bab05c1e47eca1548d0
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-14 09:11:16 -0800

    Time: 705 ms (12.59%), Space: 66.9 MB (86.74%) - LeetHub

--- 2024-calculate-special-bonus/2024-calculate-special-bonus.py
+++ 2024-calculate-special-bonus/2024-calculate-special-bonus.py
@@ -5,4 +5,9 @@ def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
 
     employees.loc[(~employees["name"].str.startswith("M")) & (employees["employee_id"] %2 == 1), "bonus"] = employees["salary"]
     return employees[["employee_id", "bonus"]].sort_values("employee_id", ascending=True)
+
+
+# def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+#     employees["bonus"]=employees["salary"]* ((employees["employee_id"]%2!=0)&(~ employees["name"].str.startswith("M")))
+#     return employees[["employee_id","bonus"]].sort_values(by='employee_id')
     
