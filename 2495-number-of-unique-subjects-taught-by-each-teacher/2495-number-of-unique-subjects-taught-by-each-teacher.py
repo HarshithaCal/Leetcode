efaba5e4c355bf0750a24886ad0c5d7b3e2a8d86
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-16 10:08:13 -0800

    Time: 574 ms (20.31%), Space: 67.7 MB (49.15%) - LeetHub

---
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
+    result = teacher.groupby(["teacher_id"])["subject_id"].nunique().reset_index()
+    return result.rename(columns={"subject_id":"cnt"})
+     
+
+
+
+    
+    
