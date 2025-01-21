dad7064a4524ec79b905acca4c0c9b6cd2e184e5
Harshitha Dwaracherla <harshitha@berkeley.edu>
2025-01-20 16:05:41 -0800

    Time: 7 ms (90.4%), Space: 18 MB (23.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,27 +1,28 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # size of freq wont be greater than 26 so the SC = O(1)
-        # TC - O(N)
-        freq = {}
-        if len(s)!= len(t):
-            return False
+        return Counter(s)==Counter(t)
+        # # size of freq wont be greater than 26 so the SC = O(1)
+        # # TC - O(N)
+        # freq = {}
+        # if len(s)!= len(t):
+        #     return False
         
-        else:
-            for i in range(len(s)):
-                if s[i] in freq:
-                    freq[s[i]] += 1
-                else:
-                    freq[s[i]] = 1
+        # else:
+        #     for i in range(len(s)):
+        #         if s[i] in freq:
+        #             freq[s[i]] += 1
+        #         else:
+        #             freq[s[i]] = 1
             
-                if t[i] in freq:
-                    freq[t[i]] -= 1
+        #         if t[i] in freq:
+        #             freq[t[i]] -= 1
                 
-                else:
-                    freq[t[i]] = -1
-        for i in freq.values():
-            if i!= 0:
-                return False
-        return True
+        #         else:
+        #             freq[t[i]] = -1
+        # for i in freq.values():
+        #     if i!= 0:
+        #         return False
+        # return True
 
 
         
